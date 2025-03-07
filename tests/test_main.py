0444f2d2ee7f5274b09e7705f29b79cbd5738181
jaimergp <jaimergp@users.noreply.github.com>
2025-03-07 18:19:17 +0100

    Add tests (#2)
    * Add tests
    
    * Sync lockfile
    
    * Add more platforms

---
+++ tests/test_main.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+import pytest
+
+from external_metadata_mappings import Ecosystems, Mapping, Registry
+
+
+HERE = Path(__file__).parent
+ROOT = HERE.parent
+DATA = ROOT / "data"
+
+
+def datafile(filename: str) -> Path:
+    return DATA / filename
+
+
+REGISTRY_PATH = datafile("registry.json")
+ECOSYSTEMS_PATH = datafile("known-ecosystems.json")
+MAPPINGS = sorted(DATA.glob("*.mapping.json"))
+
+
+def test_registry():
+    Registry.from_path(REGISTRY_PATH)
+
+
+def test_ecosystems():
+    Ecosystems.from_path(ECOSYSTEMS_PATH)
+
+
+@pytest.mark.parametrize("mapping", MAPPINGS)
+def test_mappings(mapping):
+    Mapping.from_path(mapping)
