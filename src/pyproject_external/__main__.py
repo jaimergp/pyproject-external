977beae208fdefe1849868c6be6cfa76ee913468
jaimergp <jaimergp@users.noreply.github.com>
2025-05-26 19:00:54 +0200

    Merge py-show into pyproject_external

--- src/pyproject_external/__main__.py
+++ src/pyproject_external/__main__.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2025 Quansight Labs
 
 if __name__ == "__main__":
     import sys
--- src/pyproject_external/_cli.py
+++ src/pyproject_external/_cli.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2023 Quansight Labs
+
 import logging
 import shlex
 import tarfile
