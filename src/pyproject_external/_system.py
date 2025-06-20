49ee5dc546ec011f97e9b48fca4a7980dca41bfe
jaimergp <jaimergp@users.noreply.github.com>
2025-06-20 13:31:26 +0200

    a bit better now

--- src/pyproject_external/_system.py
+++ src/pyproject_external/_system.py
@@ -26,13 +26,16 @@ def find_ecosystem_for_package_manager(package_manager: str) -> str:
 
 
 def detect_ecosystem_and_package_manager() -> tuple[str, str]:
-    if conda_prefix := os.environ.get("CONDA_PREFIX"):
+    if os.environ.get("CONDA_PREFIX"):
         # An active conda environment is present; probably want to use that
-        if Path(conda_prefix).name == os.environ.get("PIXI_ENVIRONMENT_NAME"):
-            return "conda-forge", "pixi"
-        for name in ("conda", "mamba", "micromamba"):
-            if shutil.which(name):
-                return "conda-forge", name
+        for tool in ("conda", "pixi", "mamba"):
+            if exe := os.environ.get(f"{tool.upper()}_EXE"):
+                exe = Path(exe)
+                if exe.is_file():
+                    if exe.stem == "micromamba":
+                        return "conda-forge", "micromamba"
+                    return "conda-forge", tool
+
     platform_system = platform.system()
     if platform_system == "Linux":
         distro_id = distro.id()
@@ -40,14 +43,19 @@ def detect_ecosystem_and_package_manager() -> tuple[str, str]:
             mapping = default_ecosystems().get_mapping(name, default=None)
             if mapping:
                 return name, mapping.package_managers[0]["name"]
-
         raise ValueError(f"No support for platform '{distro_id}' yet!")
+
     if platform_system == "Darwin":
-        return "homebrew", "brew"
+        if shutil.which("brew"):
+            return "homebrew", "brew"
+        raise ValueError("Only homebrew is supported on macOS!")
+
     if platform_system == "Windows" or platform_system.lower().startswith(("cygwin", "msys")):
         return "vcpkg", "vcpkg"  # TODO: Determine which one has the most complete mapping
+
     # Fallback to the conda ecosystem if available, even if no active environments are found
     for name in ("conda", "pixi", "mamba", "micromamba"):
         if shutil.which(name):
             return "conda-forge", name
+
     raise ValueError(f"No support for platform '{distro_id}' yet!")
