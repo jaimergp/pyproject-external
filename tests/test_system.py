ec0c00263b848e786598dd7aa51d059aa13439f2
jaimergp <jaimergp@users.noreply.github.com>
2025-06-20 14:14:57 +0200

    monkeypatch CONDA_PREFIX out

--- tests/test_system.py
+++ tests/test_system.py
@@ -10,15 +10,18 @@ from pyproject_external import detect_ecosystem_and_package_manager
     distro.id() != "ubuntu",
     reason="Only for Ubuntu",
 )
-def test_ubuntu():
+def test_ubuntu(monkeypatch):
+    monkeypatch.delenv("CONDA_PREFIX")
     assert detect_ecosystem_and_package_manager() == ("ubuntu", "apt")
 
 
 @pytest.mark.skipif(sys.platform != "darwin", reason="Only for macOS")
-def test_macos():
+def test_macos(monkeypatch):
+    monkeypatch.delenv("CONDA_PREFIX")
     assert detect_ecosystem_and_package_manager() == ("homebrew", "brew")
 
 
 @pytest.mark.skipif(sys.platform != "win32", reason="Only for Windows")
-def test_windows():
+def test_windows(monkeypatch):
+    monkeypatch.delenv("CONDA_PREFIX")
     assert detect_ecosystem_and_package_manager() == ("vcpkg", "vcpkg")
