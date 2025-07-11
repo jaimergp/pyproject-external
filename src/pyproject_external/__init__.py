7069e03ae4906ff72f0a26805ce015758df43462
jaimergp <jaimergp@users.noreply.github.com>
2025-07-11 08:16:45 +0200

    remove importerror catch

--- src/pyproject_external/__init__.py
+++ src/pyproject_external/__init__.py
@@ -13,11 +13,7 @@ from ._system import (  # noqa
     activated_conda_env,
 )
 from ._url import DepURL  # noqa
-
-try:
-    from ._version import __version__
-except ImportError:
-    __version__ = "0.0.0.dev0+placeholder"
+from ._version import __version__
 
 __all__ = [
     "__version__",
